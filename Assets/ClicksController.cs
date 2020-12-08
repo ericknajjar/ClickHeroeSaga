b5f6ffbd40fa0f406ba30cbce7fa995e22e60751
Erick Najjar <enajjar@gazeus.com>
2020-12-08 15:17:55 -0300

    persistencia

--- Assets/ClicksController.cs
+++ Assets/ClicksController.cs
@@ -7,16 +7,22 @@ public class ClicksController : MonoBehaviour
 {
     [SerializeField] Text _display;
     uint _totalClicks;
+    const string KEY = "ClicksController._totalClicks";
 
     void Start()
     {
-        
+        string clickString = PlayerPrefs.GetString(KEY, "0");
+
+        _totalClicks = uint.Parse(clickString);
+        _display.text = clickString;
     }
 
     public void Click()
     {
         ++_totalClicks;
-        _display.text = _totalClicks.ToString();
+        string toShow = _totalClicks.ToString();
+        _display.text = toShow;
+        PlayerPrefs.SetString(KEY, toShow);
     }
 
 }
