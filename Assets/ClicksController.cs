c872d44d783ba5ade8a62b8381eead5a832aa988
Erick Najjar <enajjar@gazeus.com>
2020-12-08 14:41:28 -0300

    click básico

---
+++ Assets/ClicksController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClicksController : MonoBehaviour
+{
+    [SerializeField] Text _display;
+    uint _totalClicks;
+
+    void Start()
+    {
+        
+    }
+
+    public void Click()
+    {
+        ++_totalClicks;
+        _display.text = _totalClicks.ToString();
+    }
+
+}
