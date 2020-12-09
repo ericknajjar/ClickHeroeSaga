f3102adebd6f886bc07a40bd16f606f6e01f1443
Erick Najjar <enajjar@gazeus.com>
2020-12-09 20:31:03 -0300

    login e save funcionando

--- Assets/ClicksController.cs
+++ Assets/ClicksController.cs
@@ -1,20 +1,23 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class ClicksController : MonoBehaviour
 {
     [SerializeField] Text _display;
-    uint _totalClicks;
-    const string KEY = "ClicksController._totalClicks";
+    public static uint _totalClicks;
+    public static string _userId;
+    public static string _accessToken;
+
 
     void Start()
     {
-        string clickString = PlayerPrefs.GetString(KEY, "0");
-
-        _totalClicks = uint.Parse(clickString);
-        _display.text = clickString;
+    
+        _display.text = _totalClicks.ToString();
     }
 
     public void Click()
@@ -22,7 +25,41 @@ public class ClicksController : MonoBehaviour
         ++_totalClicks;
         string toShow = _totalClicks.ToString();
         _display.text = toShow;
-        PlayerPrefs.SetString(KEY, toShow);
+        StartCoroutine(SendUpdatedClicks());
+   
+    }
+
+    IEnumerator SendUpdatedClicks()
+    {
+        var data = new Dictionary<string, object>
+        {
+            { "user_id", _userId },
+            { "token",_accessToken},
+            { "clicks",_totalClicks},
+        };
+
+        string toSend = JsonConvert.SerializeObject(data);
+        byte[] bytesToSend = Encoding.UTF8.GetBytes(toSend);
+
+        using (UnityWebRequest www = new UnityWebRequest("http://localhost:5000/set_clicks", UnityWebRequest.kHttpVerbPOST))
+        {
+            www.SetRequestHeader("Content-Type", "application/json");
+            www.uploadHandler = new UploadHandlerRaw(bytesToSend);
+            www.downloadHandler = new DownloadHandlerBuffer();
+
+            yield return www.SendWebRequest();
+
+            var text = www.downloadHandler.text;
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else if (www.responseCode != 200)
+            {
+                Debug.LogError($"{www.responseCode}: {text}");
+            }
+         
+        }
     }
 
 }
---
+++ Assets/LoginControler.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginControler : MonoBehaviour
+{
+    [SerializeField] InputField _email;
+    [SerializeField] InputField _password;
+
+    public void OnLogin()
+    {
+        // Debug.Log($"{_email.text} {_password.text}");
+        StartCoroutine(LoginCorrotine());
+       
+    }
+
+    IEnumerator LoginCorrotine()
+    {
+        var data = new Dictionary<string, string>
+        {
+            { "email", _email.text },
+            { "password",_password.text},
+        };
+
+        string toSend = JsonConvert.SerializeObject(data);
+        byte[] bytesToSend = Encoding.UTF8.GetBytes(toSend);
+
+        Debug.Log($"Sending... {toSend}");
+        using (UnityWebRequest www = new UnityWebRequest("http://localhost:5000/pior_login_do_mundo", UnityWebRequest.kHttpVerbPOST))
+        {
+            www.SetRequestHeader("Content-Type", "application/json");
+            www.uploadHandler = new UploadHandlerRaw(bytesToSend);
+            www.downloadHandler = new DownloadHandlerBuffer();
+
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                var text = www.downloadHandler.text;
+                if (www.responseCode == 200)
+                {
+                    var result = JObject.Parse(text);
+
+                    ClicksController._userId = result["user_id"].Value<string>();
+                    ClicksController._accessToken = result["token"].Value<string>();
+                    ClicksController._totalClicks = result["clicks"].Value<uint>();
+                    Debug.Log(result);
+
+                    SceneManager.LoadScene(1);
+
+                   
+                }
+                else
+                {
+                    Debug.LogError($"{www.responseCode}: {text}");
+                }
+            }
+        }
+    }
+}
